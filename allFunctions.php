927262ea3b7b9971c9fd26590eb9776f58778e85
Oural <71041046+Oural1206@users.noreply.github.com>
2022-04-20 10:21:45 +0200

    Update allFunctions.php

--- allFunctions.php
+++ allFunctions.php
@@ -1,3 +1,23 @@
 <?php
+$repositoryName = "php-functions";
 
+$link = "https://github.com/Oural1206/$repositoryName/archive/refs/heads/main.zip";
+$file = file_put_contents("repository.zip", file_get_contents($link));
+$zip = new ZipArchive;
+$zip -> open("repository.zip");
+$zip -> extractTo(".");
+$zip -> close();
+unlink("repository.zip");
+
+$repository = "$repositoryName-main";
+$functionsDir = "$repository/functions";
+
+foreach (scandir($functionsDir) as $function) {
+    if ($function != "." && $function != "..") {
+        $path = "$functionsDir/$function";
+        include($path);
+    }
+}
+
+remove($repository);
 ?>
