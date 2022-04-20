f37c332f9ec5d848edc3d62730f43b64ce8e9514
Jules Jardri <jules.jardri@icloud.com>
2022-04-20 10:04:54 +0200

    Add remove functions

---
+++ functions/remove.php
@@ -0,0 +1,12 @@
+function remove($path) {
+    if (is_dir($path)) {
+        foreach (scandir($path) as $element) {
+            if ($element != "." && $element != "..") {
+                remove(getPath([$path, $element]));
+            }
+        }
+        rmdir($path);
+    } else {
+        unlink($path);
+    }
+}
